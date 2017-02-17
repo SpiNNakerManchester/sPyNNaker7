f76b6e236290084931a9d4f1332b90f0f897aa43
Alan <alan.barry.stokes@gmail.com>
2017-02-17 17:18:10 -0500

    added what i think is the right setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+exec (open("spynnaker/pyNN/_version.py").read())
+
+setup(
+    name="sPyNNaker",
+    version=__version__,
+    description="Spinnaker implementation of PyNN 0.7",
+    url="https://github.com/SpiNNakerManchester/SpyNNaker7",
+    packages=['spynnaker7',
+              'spynnaker7.pyNN',
+              'spynnaker7.pyNN.models',
+              'spynnaker7.pyNN.utilities',
+              'spynnaker7.pyNN.utilities.conf'],
+    install_requires=[
+        'SpiNNFrontEndCommon >= 3.0.0, < 4.0.0', 'sPyNNaker'
+                                                 'pyNN >= 0.7, < 0.8', 'numpy',
+        'scipy', 'lxml', 'six', 'bitarray']
+)
