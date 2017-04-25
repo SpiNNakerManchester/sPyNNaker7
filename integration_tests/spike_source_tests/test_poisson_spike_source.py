b79e42347d0b2f9b730c4bdf2c9909ed14f27f12
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-04-25 10:48:49 +0100

    moved intergration tests out of unittests

--- integration_tests/spike_source_tests/test_poisson_spike_source.py
+++ integration_tests/spike_source_tests/test_poisson_spike_source.py
@@ -1,15 +1,16 @@
 import unittest
-import spynnaker.pyNN as p
-import numpy
-import random
+import spynnaker7.pyNN as p
 
 
 class MyTestCase(unittest.TestCase):
-    __name__ = "bOB"
+    @unittest.skip("Not implemented")
+    def test_something(self):
+        self.assertEqual(True, False, "Test not implemented yet")
 
-    def test_recording_1_element(self):
+    @unittest.skip("Not implemented")
+    def test_recording_poisson_spikes(self):
         p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 200  # number of neurons in each population
+        n_neurons = 256  # number of neurons in each population
         p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
@@ -26,61 +27,10 @@ class MyTestCase(unittest.TestCase):
         populations = list()
         projections = list()
 
-        spike_array = {'spike_times': [[0]]}
         populations.append(p.Population(n_neurons, p.IF_curr_exp,
                                         cell_params_lif,
                                         label='pop_1'))
-        populations.append(p.Population(1, p.SpikeSourceArray, spike_array,
-                                        label='inputSpikes_1'))
-
-        projections.append(p.Projection(populations[1], populations[0],
-                                        p.AllToAllConnector()))
-
-        populations[1].record()
-
-        p.run(5000)
-
-        spike_array_spikes = populations[1].getSpikes()
-        boxed_array = numpy.zeros(shape=(0, 2))
-        boxed_array = numpy.append(boxed_array, [[0, 0]], axis=0)
-        numpy.testing.assert_array_equal(spike_array_spikes, boxed_array)
-
-        p.end()
-
-    def test_recording_numerious_element(self):
-        p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 20  # number of neurons in each population
-        p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
-
-        cell_params_lif = {'cm': 0.25,
-                           'i_offset': 0.0,
-                           'tau_m': 20.0,
-                           'tau_refrac': 2.0,
-                           'tau_syn_E': 5.0,
-                           'tau_syn_I': 5.0,
-                           'v_reset': -70.0,
-                           'v_rest': -65.0,
-                           'v_thresh': -50.0
-                           }
-
-        populations = list()
-        projections = list()
-
-        boxed_array = numpy.zeros(shape=(0, 2))
-        spike_array = list()
-        for neuron_id in range(0, n_neurons):
-            spike_array.append(list())
-            for random_time in range(0, 20):
-                random_time2 = random.randint(0, 5000)
-                boxed_array = numpy.append(
-                    boxed_array, [[neuron_id, random_time2]], axis=0)
-                spike_array[neuron_id].append(random_time)
-        spike_array_params = {'spike_times': spike_array}
-        populations.append(p.Population(n_neurons, p.IF_curr_exp,
-                                        cell_params_lif,
-                                        label='pop_1'))
-        populations.append(p.Population(n_neurons, p.SpikeSourceArray,
-                                        spike_array_params,
+        populations.append(p.Population(n_neurons, p.SpikeSourcePoisson, {},
                                         label='inputSpikes_1'))
 
         projections.append(p.Projection(populations[1], populations[0],
@@ -90,15 +40,15 @@ class MyTestCase(unittest.TestCase):
 
         p.run(5000)
 
-        spike_array_spikes = populations[1].getSpikes()
-        boxed_array = boxed_array[numpy.lexsort((boxed_array[:, 1],
-                                                 boxed_array[:, 0]))]
-        numpy.testing.assert_array_equal(spike_array_spikes, boxed_array)
+        spikes = populations[1].getSpikes()
+        print spikes
+
         p.end()
 
-    def test_recording_numerious_element_over_limit(self):
+    @unittest.skip("Not implemented")
+    def test_recording_poisson_large_spikes(self):
         p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 2000  # number of neurons in each population
+        n_neurons = 2560  # number of neurons in each population
         p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
@@ -115,21 +65,10 @@ class MyTestCase(unittest.TestCase):
         populations = list()
         projections = list()
 
-        boxed_array = numpy.zeros(shape=(0, 2))
-        spike_array = list()
-        for neuron_id in range(0, n_neurons):
-            spike_array.append(list())
-            for random_time in range(0, 200000):
-                random_time2 = random.randint(0, 50000)
-                boxed_array = numpy.append(
-                    boxed_array, [[neuron_id, random_time2]], axis=0)
-                spike_array[neuron_id].append(random_time)
-        spike_array_params = {'spike_times': spike_array}
         populations.append(p.Population(n_neurons, p.IF_curr_exp,
                                         cell_params_lif,
                                         label='pop_1'))
-        populations.append(p.Population(n_neurons, p.SpikeSourceArray,
-                                        spike_array_params,
+        populations.append(p.Population(n_neurons, p.SpikeSourcePoisson, {},
                                         label='inputSpikes_1'))
 
         projections.append(p.Projection(populations[1], populations[0],
@@ -137,12 +76,49 @@ class MyTestCase(unittest.TestCase):
 
         populations[1].record()
 
-        p.run(50000)
+        p.run(5000)
 
-        spike_array_spikes = populations[1].getSpikes()
-        boxed_array = boxed_array[numpy.lexsort((boxed_array[:, 1],
-                                                 boxed_array[:, 0]))]
-        numpy.testing.assert_array_equal(spike_array_spikes, boxed_array)
+        spikes = populations[1].getSpikes()
+        print spikes
+
+        p.end()
+
+    def test_recording_poisson_spikes_rate_0(self):
+        p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
+        n_neurons = 256  # number of neurons in each population
+        p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
+
+        cell_params_lif = {'cm': 0.25,
+                           'i_offset': 0.0,
+                           'tau_m': 20.0,
+                           'tau_refrac': 2.0,
+                           'tau_syn_E': 5.0,
+                           'tau_syn_I': 5.0,
+                           'v_reset': -70.0,
+                           'v_rest': -65.0,
+                           'v_thresh': -50.0
+                           }
+
+        populations = list()
+        projections = list()
+
+        populations.append(p.Population(n_neurons, p.IF_curr_exp,
+                                        cell_params_lif,
+                                        label='pop_1'))
+        populations.append(p.Population(n_neurons, p.SpikeSourcePoisson,
+                                        {'rate': 0},
+                                        label='inputSpikes_1'))
+
+        projections.append(p.Projection(populations[1], populations[0],
+                                        p.OneToOneConnector()))
+
+        populations[1].record()
+
+        p.run(5000)
+
+        spikes = populations[1].getSpikes()
+        print spikes
+
         p.end()
 
 
--- integration_tests/spike_source_tests/test_spike_source_array.py
+++ integration_tests/spike_source_tests/test_spike_source_array.py
@@ -1,16 +1,15 @@
 import unittest
-import spynnaker.pyNN as p
+import spynnaker7.pyNN as p
+import numpy
+import random
 
 
 class MyTestCase(unittest.TestCase):
-    @unittest.skip("Not implemented")
-    def test_something(self):
-        self.assertEqual(True, False, "Test not implemented yet")
+    __name__ = "bOB"
 
-    @unittest.skip("Not implemented")
-    def test_recording_poisson_spikes(self):
+    def test_recording_1_element(self):
         p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 256  # number of neurons in each population
+        n_neurons = 200  # number of neurons in each population
         p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
@@ -27,28 +26,30 @@ class MyTestCase(unittest.TestCase):
         populations = list()
         projections = list()
 
+        spike_array = {'spike_times': [[0]]}
         populations.append(p.Population(n_neurons, p.IF_curr_exp,
                                         cell_params_lif,
                                         label='pop_1'))
-        populations.append(p.Population(n_neurons, p.SpikeSourcePoisson, {},
+        populations.append(p.Population(1, p.SpikeSourceArray, spike_array,
                                         label='inputSpikes_1'))
 
         projections.append(p.Projection(populations[1], populations[0],
-                                        p.OneToOneConnector()))
+                                        p.AllToAllConnector()))
 
         populations[1].record()
 
         p.run(5000)
 
-        spikes = populations[1].getSpikes()
-        print spikes
+        spike_array_spikes = populations[1].getSpikes()
+        boxed_array = numpy.zeros(shape=(0, 2))
+        boxed_array = numpy.append(boxed_array, [[0, 0]], axis=0)
+        numpy.testing.assert_array_equal(spike_array_spikes, boxed_array)
 
         p.end()
 
-    @unittest.skip("Not implemented")
-    def test_recording_poisson_large_spikes(self):
+    def test_recording_numerious_element(self):
         p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 2560  # number of neurons in each population
+        n_neurons = 20  # number of neurons in each population
         p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
@@ -65,10 +66,21 @@ class MyTestCase(unittest.TestCase):
         populations = list()
         projections = list()
 
+        boxed_array = numpy.zeros(shape=(0, 2))
+        spike_array = list()
+        for neuron_id in range(0, n_neurons):
+            spike_array.append(list())
+            for random_time in range(0, 20):
+                random_time2 = random.randint(0, 5000)
+                boxed_array = numpy.append(
+                    boxed_array, [[neuron_id, random_time2]], axis=0)
+                spike_array[neuron_id].append(random_time)
+        spike_array_params = {'spike_times': spike_array}
         populations.append(p.Population(n_neurons, p.IF_curr_exp,
                                         cell_params_lif,
                                         label='pop_1'))
-        populations.append(p.Population(n_neurons, p.SpikeSourcePoisson, {},
+        populations.append(p.Population(n_neurons, p.SpikeSourceArray,
+                                        spike_array_params,
                                         label='inputSpikes_1'))
 
         projections.append(p.Projection(populations[1], populations[0],
@@ -78,14 +90,15 @@ class MyTestCase(unittest.TestCase):
 
         p.run(5000)
 
-        spikes = populations[1].getSpikes()
-        print spikes
-
+        spike_array_spikes = populations[1].getSpikes()
+        boxed_array = boxed_array[numpy.lexsort((boxed_array[:, 1],
+                                                 boxed_array[:, 0]))]
+        numpy.testing.assert_array_equal(spike_array_spikes, boxed_array)
         p.end()
 
-    def test_recording_poisson_spikes_rate_0(self):
+    def test_recording_numerious_element_over_limit(self):
         p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 256  # number of neurons in each population
+        n_neurons = 2000  # number of neurons in each population
         p.set_number_of_neurons_per_core("IF_curr_exp", n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
@@ -102,11 +115,21 @@ class MyTestCase(unittest.TestCase):
         populations = list()
         projections = list()
 
+        boxed_array = numpy.zeros(shape=(0, 2))
+        spike_array = list()
+        for neuron_id in range(0, n_neurons):
+            spike_array.append(list())
+            for random_time in range(0, 200000):
+                random_time2 = random.randint(0, 50000)
+                boxed_array = numpy.append(
+                    boxed_array, [[neuron_id, random_time2]], axis=0)
+                spike_array[neuron_id].append(random_time)
+        spike_array_params = {'spike_times': spike_array}
         populations.append(p.Population(n_neurons, p.IF_curr_exp,
                                         cell_params_lif,
                                         label='pop_1'))
-        populations.append(p.Population(n_neurons, p.SpikeSourcePoisson,
-                                        {'rate': 0},
+        populations.append(p.Population(n_neurons, p.SpikeSourceArray,
+                                        spike_array_params,
                                         label='inputSpikes_1'))
 
         projections.append(p.Projection(populations[1], populations[0],
@@ -114,11 +137,12 @@ class MyTestCase(unittest.TestCase):
 
         populations[1].record()
 
-        p.run(5000)
+        p.run(50000)
 
-        spikes = populations[1].getSpikes()
-        print spikes
-
+        spike_array_spikes = populations[1].getSpikes()
+        boxed_array = boxed_array[numpy.lexsort((boxed_array[:, 1],
+                                                 boxed_array[:, 0]))]
+        numpy.testing.assert_array_equal(spike_array_spikes, boxed_array)
         p.end()
 
 
