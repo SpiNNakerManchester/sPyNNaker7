80a3cf6b5f2885324a83f6025d3d1cb72ef3f0d9
Christian <brenninc@cs.man.ac.uk>
2017-05-08 09:43:19 +0100

    test fix

--- p7_integration_tests/test_1_0_time_steps/test_gsyn/test_synfire_1dot0_timestep_test_print_gsyn.py
+++ p7_integration_tests/test_1_0_time_steps/test_gsyn/test_synfire_1dot0_timestep_test_print_gsyn.py
@@ -32,6 +32,7 @@ class TestPrintGsyn(BaseTestCase):
                            neurons_per_core=neurons_per_core, delay=delay,
                            run_times=[runtime], gsyn_path=gsyn_path)
         spikes = synfire_run.get_output_pop_spikes()
+        gsyn = synfire_run.get_output_pop_gsyn()
 
         self.assertEquals(56, len(spikes))
         spike_checker.synfire_spike_checker(spikes, n_neurons)
