347fab29a922981956b1e9cc26fb25fd957ee5b7
Christian <brenninc@cs.man.ac.uk>
2017-05-08 14:22:26 +0100

    reset set_model_max_atoms_per_core before each test

--- p7_integration_tests/base_test_case.py
+++ p7_integration_tests/base_test_case.py
@@ -4,6 +4,8 @@ import unittest
 
 import spynnaker7.pyNN.utilities.conf as conf
 from spynnaker7.pyNN.spinnaker import Spinnaker as Spinnaker
+from spynnaker.pyNN.models.neuron.builds.if_curr_exp_base \
+    import IFCurrExpBase as IF_curr_exp
 
 
 class BaseTestCase(unittest.TestCase):
@@ -14,9 +16,12 @@ class BaseTestCase(unittest.TestCase):
         os.chdir(path)
         config = conf.load_config()
         Spinnaker._set_config(config)
+        # Some tests changes this so change it back here!
+        IF_curr_exp.set_model_max_atoms_per_core()
 
     def assert_logs_error(self, log_records, sub_message):
         for record in log_records:
+            print record
             if record.levelname == 'ERROR':
                 if sub_message in record.msg:
                     return
