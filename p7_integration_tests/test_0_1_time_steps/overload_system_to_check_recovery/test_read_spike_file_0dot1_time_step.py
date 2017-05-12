0b7af8a8090d9fcf6c70e6de15a0ef76f8c791c0
Christian <brenninc@cs.man.ac.uk>
2017-05-12 13:28:01 +0100

    Check for overloading system recovery

---
+++ p7_integration_tests/test_0_1_time_steps/overload_system_to_check_recovery/test_read_spike_file_0dot1_time_step.py
@@ -0,0 +1,78 @@
+# spynnaker imports
+import spynnaker7.pyNN as p
+from p7_integration_tests.base_test_case import BaseTestCase
+from spinnman.exceptions import SpinnmanTimeoutException
+
+# general imports
+import os
+
+
+def do_run():
+    """
+    test that tests the printing of v from a pre determined recording
+    :return:
+    """
+    p.setup(timestep=0.04, min_delay=1.0, max_delay=4.0)
+    n_neurons = 128 * 128  # number of neurons in each population
+    p.set_number_of_neurons_per_core("IF_cond_exp", 256)
+
+    cell_params_lif = {'cm': 0.25,
+                       'i_offset': 0.0,
+                       'tau_m': 20.0,
+                       'tau_refrac': 2.0,
+                       'tau_syn_E': 5.0,
+                       'tau_syn_I': 5.0,
+                       'v_reset': -70.0,
+                       'v_rest': -65.0,
+                       'v_thresh': -50.0,
+                       'e_rev_E': 0.,
+                       'e_rev_I': -80.
+                       }
+
+    populations = list()
+    projections = list()
+
+    weight_to_spike = 0.035
+    delay = 1.7
+
+    current_file_path = os.path.dirname(os.path.abspath(__file__))
+    spikes_file = os.path.join(current_file_path, 'test.spikes')
+
+    spikes = read_spikefile(spikes_file, n_neurons)
+    print spikes
+    spike_array = {'spike_times': spikes}
+
+    populations.append(p.Population(
+        n_neurons, p.SpikeSourceArray, spike_array,
+        label='inputSpikes_1'))
+    populations.append(p.Population(
+        n_neurons, p.IF_cond_exp, cell_params_lif, label='pop_1'))
+    projections.append(p.Projection(
+        populations[0], populations[1], p.OneToOneConnector(
+            weights=weight_to_spike, delays=delay)))
+    populations[1].record()
+
+    p.run(1000)
+
+    spikes = populations[1].getSpikes(compatible_output=True)
+
+    p.end()
+
+    return spikes
+
+
+class TestReadingSpikeArrayDataAndBigSlices(BaseTestCase):
+    """
+    tests the printing of print v given a simulation
+    """
+    def test_script(self):
+        """
+        test that tests the printing of v from a pre determined recording
+        :return:
+        """
+        with self.assertRaises(SpinnmanTimeoutException):
+            do_run()
+
+
+if __name__ == '__main__':
+    spikes = do_run()
