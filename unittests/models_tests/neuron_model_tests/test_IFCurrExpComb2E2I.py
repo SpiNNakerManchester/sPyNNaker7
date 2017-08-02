d3fad6ea0af79547e714c6e4ea612589802c0fb7
simond68 <simon.davidson@manchester.ac.uk>
2017-08-02 14:41:54 +0100

    update 2E2I test

--- unittests/models_tests/neuron_model_tests/test_IFCurrExpComb2E2I.py
+++ unittests/models_tests/neuron_model_tests/test_IFCurrExpComb2E2I.py
@@ -1,5 +1,5 @@
 import spynnaker7.pyNN as p
-import python.plot_utils as plot_utils
+# import python.plot_utils as plot_utils
 p.setup(.1)
 
 pop_src = p.Population(1, p.SpikeSourceArray, {'spike_times': [[1, 4]]}, label="src")
@@ -30,7 +30,7 @@ v = pop_ex.get_v()
 curr = pop_ex.get_gsyn()
 spikes = pop_ex.getSpikes()
 
-plot_utils.plotAll(v, spikes)
-plot_utils.plot_gsyn(curr)
+#plot_utils.plotAll(v, spikes)
+#plot_utils.plot_gsyn(curr)
 p.end()
 print "\n job done"
