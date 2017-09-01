d543d6c9d7c57fa110ed187a0675f043dfdfe1ba
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-09-01 16:10:59 +0100

    Update to add version information

--- spynnaker7/pyNN/spinnaker.py
+++ spynnaker7/pyNN/spinnaker.py
@@ -3,6 +3,8 @@ import logging
 
 from pyNN.random import NumpyRNG
 from pyNN.random import RandomDistribution
+from pyNN import __version__ as pynn_version
+
 from spinn_front_end_common.utilities import globals_variables
 from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
 from spynnaker7.pyNN.models.pynn_population import Population
@@ -31,8 +33,8 @@ class Spinnaker(AbstractSpiNNakerCommon, Spynnaker7SimulatorInterface):
             self, host_name=None, timestep=None, min_delay=None,
             max_delay=None, graph_label=None, database_socket_addresses=None,
             n_chips_required=None):
-        # Determine default executable folder location
-        # and add this default to end of list of search paths
+        front_end_versions = [("sPyNNaker7_version", version)]
+        front_end_versions.append(("pyNN_version", pynn_version))
 
         # population holders
         AbstractSpiNNakerCommon.__init__(
@@ -40,7 +42,7 @@ class Spinnaker(AbstractSpiNNakerCommon, Spynnaker7SimulatorInterface):
             graph_label=graph_label,
             n_chips_required=n_chips_required, timestep=timestep,
             hostname=host_name, max_delay=max_delay, min_delay=min_delay,
-            front_end_name="sPyNNaker7 version {}".format(version))
+            front_end_versions=front_end_versions)
 
     def create_population(self, size, cellclass, cellparams, structure, label):
         """ creates a pynn 0.75 population
