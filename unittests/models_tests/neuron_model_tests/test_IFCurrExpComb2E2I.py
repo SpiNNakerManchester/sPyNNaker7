3a61575681baee91cbc0057496327eaa30cf6911
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2017-09-08 17:53:05 +0100

    Update 2E2I test for repeated spikes

--- unittests/models_tests/neuron_model_tests/test_IFCurrExpComb2E2I.py
+++ unittests/models_tests/neuron_model_tests/test_IFCurrExpComb2E2I.py
@@ -2,7 +2,7 @@ import spynnaker7.pyNN as p
 import python.plot_utils as plot_utils
 p.setup(.1)
 
-pop_src = p.Population(1, p.SpikeSourceArray, {'spike_times': [[0]]}, label="src")
+pop_src = p.Population(1, p.SpikeSourceArray, {'spike_times': [[0, 100]]}, label="src")
 
 pop_ex = p.Population(1, p.extra_models.IF_curr_comb_exp_2E2I, {}, label="test")
 
@@ -19,11 +19,10 @@ inh_proj = p.Projection(pop_src, pop_ex,
 inh2_proj = p.Projection(pop_src, pop_ex,
         p.OneToOneConnector(weights=1, delays=40), target="inhibitory2")
 
-
 pop_ex.record()
 pop_ex.record_gsyn()
 pop_ex.record_v()
-p.run(100)
+p.run(200)
 
 v = pop_ex.get_v()
 curr = pop_ex.get_gsyn()
