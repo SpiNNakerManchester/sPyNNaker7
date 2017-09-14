b73c67d9c987c348c9bcf2f998f5c74ba4e17bfa
Garibaldi Pineda Garcia <chanokin@gmail.com>
2017-09-14 11:23:55 +0100

    added neuromodulation

--- spynnaker7/pyNN/models/plasticity_components/timing_dependence/timing_dependence_spike_pair.py
+++ spynnaker7/pyNN/models/plasticity_components/timing_dependence/timing_dependence_spike_pair.py
@@ -5,7 +5,7 @@ from spynnaker.pyNN.models.neuron.plasticity.stdp.timing_dependence.\
 
 class TimingDependenceSpikePair(CommonTimingDependenaceSpikePair):
 
-    def __init__(self, tau_plus=20.0, tau_minus=20.0, tau_c=1000, tau_d=200):
+    def __init__(self, tau_plus=20.0, tau_minus=20.0, tau_c=None, tau_d=None):
         CommonTimingDependenaceSpikePair.__init__(
             self, tau_plus=tau_plus, tau_minus=tau_minus,
             tau_c=tau_c, tau_d = tau_d)
