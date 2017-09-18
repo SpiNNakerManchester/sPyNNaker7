2a42e3ab6525238303839e9167f0b2983527800e
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-09-18 14:26:01 +0100

    list all spinnaker dependencies

--- setup.py
+++ setup.py
@@ -39,6 +39,14 @@ setup(
     packages=packages,
     package_data=package_data,
     install_requires=[
+        'SpiNNUtilities >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNStorageHandlers >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNMachine >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNMan >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNaker_PACMAN >= 1!4.0.0a5, < 1!5.0.0',
+        'SpiNNaker_DataSpecification >= 1!4.0.0a5, < 1!5.0.0',
+        'spalloc >= 0.2.2, < 1.0.0',
+        'SpiNNFrontEndCommon >= 1!4.0.0a5, < 1!5.0.0',
         'sPyNNaker >= 1!4.0.0a5, < 1!5.0.0',
         'pyNN >= 0.7, < 0.8']
 )
