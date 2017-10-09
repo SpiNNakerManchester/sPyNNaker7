7c193cdcf7f60eb27e85c5b3747395f506941fc7
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2017-10-09 14:31:02 +0100

    flake8

--- unittests/models_tests/neuron_model_tests/use_IFCurrExpAlpha.py
+++ unittests/models_tests/neuron_model_tests/use_IFCurrExpAlpha.py
@@ -2,21 +2,25 @@ import spynnaker7.pyNN as p
 import python.plot_utils as plot_utils
 p.setup(0.1)
 
-pop_src1 = p.Population(1, p.SpikeSourceArray, {'spike_times': [[5, 15, 20, 30]]}, label="src1")
-
+pop_src1 = p.Population(1, p.SpikeSourceArray,
+                        {'spike_times': [[5, 10, 15, 20]]}, label="src1")
 
 pop_ex = p.Population(1, p.IF_curr_alpha, {}, label="test")
-pop_ex.set("exc_tau", 2)
+# pop_ex.set("tau_syn_E", 3)
+# pop_ex.set("tau_syn_I", 4)
+
 # define the projection
 exc_proj = p.Projection(pop_src1, pop_ex,
-        p.OneToOneConnector(weights=1, delays=1), target="excitatory")
+                        p.OneToOneConnector(weights=1, delays=1),
+                        target="excitatory")
 inh_proj = p.Projection(pop_src1, pop_ex,
-        p.OneToOneConnector(weights=1, delays=100), target="inhibitory")
+                        p.OneToOneConnector(weights=1, delays=20),
+                        target="inhibitory")
 
 pop_ex.record()
 pop_ex.record_gsyn()
 pop_ex.record_v()
-p.run(200)
+p.run(50)
 
 v = pop_ex.get_v()
 curr = pop_ex.get_gsyn()
