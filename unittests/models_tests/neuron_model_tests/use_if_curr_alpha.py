fb277844607630c7eba36d975f9924e075e9201f
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2017-10-11 14:10:19 +0100

    Adjust test to match spreadsheet

--- unittests/models_tests/neuron_model_tests/use_if_curr_alpha.py
+++ unittests/models_tests/neuron_model_tests/use_if_curr_alpha.py
@@ -2,11 +2,11 @@ import spynnaker7.pyNN as p
 p.setup(0.1)
 
 pop_src1 = p.Population(1, p.SpikeSourceArray,
-                        {'spike_times': [[5, 10, 15, 20]]}, label="src1")
+                        {'spike_times': [[5, 15, 20, 30]]}, label="src1")
 
 pop_ex = p.Population(1, p.IF_curr_alpha, {}, label="test")
-# pop_ex.set("tau_syn_E", 3)
-# pop_ex.set("tau_syn_I", 4)
+pop_ex.set("tau_syn_E", 2)
+pop_ex.set("tau_syn_I", 4)
 
 # define the projection
 exc_proj = p.Projection(pop_src1, pop_ex,
