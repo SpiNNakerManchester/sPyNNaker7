59c45d0a748f76659dd3f2645b2879c2006fb113
Garibaldi Pineda Garcia <chanokin@gmail.com>
2017-11-13 12:45:33 +0000

    fixes to mapping connector, to accommodate bricks in breakout

--- spynnaker7/pyNN/models/connectors/mapping_connector.py
+++ spynnaker7/pyNN/models/connectors/mapping_connector.py
@@ -13,16 +13,15 @@ class MappingConnector(CommonMappingConnector):
     """
 
     def __init__(
-            self, width, height, channel, row_bits, channel_bits=1,
+            self, width, height, channel, height_bits, channel_bits=1,
             event_bits=0, weights=1.0, delays=1., space=Space(),
             safe=True, verbose=False, generate_on_machine=False):
         """
         """
         CommonMappingConnector.__init__(
             self, width=width, height=height, channel=channel,
-            row_bits=row_bits, channel_bits=channel_bits,
-            safe=safe, verbose=verbose,
-            random_number_class=RandomDistribution,
+            height_bits=height_bits, channel_bits=channel_bits, event_bits=event_bits,
+            safe=safe, verbose=verbose, random_number_class=RandomDistribution,
             generate_on_machine=generate_on_machine)
         self.set_weights_and_delays(weights, delays)
         self.set_space(space)
