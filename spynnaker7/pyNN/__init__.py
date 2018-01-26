4735a582db191b6c492feaaf33bbd390f6903b05
Garibaldi Pineda Garcia <chanokin@gmail.com>
2018-01-26 15:07:02 +0000

    adaptive threshold

--- spynnaker7/pyNN/__init__.py
+++ spynnaker7/pyNN/__init__.py
@@ -27,6 +27,8 @@ from spynnaker.pyNN.models.neuron.builds.izk_curr_exp_base \
     import IzkCurrExpBase as IZK_curr_exp
 from spynnaker.pyNN.models.neuron.builds.if_curr_exp_supervision \
      import IFCurrExpSupervision as IF_curr_exp_supervision
+from spynnaker.pyNN.models.neuron.builds.if_curr_exp_supervision_adaptive \
+     import IFCurrExpSupervision as IF_curr_exp_supervision_adaptive
 from spynnaker.pyNN.models.neuron.synapse_dynamics.pynn_synapse_dynamics \
     import PyNNSynapseDynamics as SynapseDynamics
 from spynnaker.pyNN.models.neuron.synapse_dynamics.synapse_dynamics_stdp \
