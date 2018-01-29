7f8dccfc535e55ccb1e3e916357d22bbe5b11be0
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-01-29 19:17:01 +0000

    update test

--- p7_integration_tests/test_neuron_models/test_voltage_dependent_input_IZK_4E4I.py
+++ p7_integration_tests/test_neuron_models/test_voltage_dependent_input_IZK_4E4I.py
@@ -5,21 +5,24 @@ p.setup(0.1)
 runtime = 1000
 populations = []
 
-t_before_pulses = 750
-num_pulses = 6
-period = 500 #ms
-amplitude = 2
-t_after_pulses = 1500
+t_before_pulses =2000
+# num_pulses = 6
+# period = 500 #ms
+# amplitude = 2
+# t_after_pulses = 1500
 
 pop_src1 = p.Population(1, p.SpikeSourceArray,
-                        {'spike_times': [105, 106, 107 ,108, 109, 110, 111, 112]#, 110, 115, 120, 125, 1450]#, 485]
+                        {'spike_times': [105],# 106, 107 ,108, 109, 110, 111, 112]#, 110, 115, 120, 125, 1450]#, 485]
                                          }, label="src1")
 pop_src2 = p.Population(1, p.SpikeSourceArray,
-                        {'spike_times': [105]#, 465, 470, 475, 480]# 750,755,760,765, 766, 767, 768]
+                        {'spike_times': [4505]#, 465, 470, 475, 480]# 750,755,760,765, 766, 767, 768]
+                         }, label="src2")
+pop_src3 = p.Population(1, p.SpikeSourceArray,
+                        {'spike_times': [100]#, 465, 470, 475, 480]# 750,755,760,765, 766, 767, 768]
                          }, label="src2")
 
 cell_params = { # L_V_Py
-                       'a': 0.111, 'c': -65.0, 'b': 0.2, 'd': 2.0, 'i_offset': 3.97,
+        'a': 0.111, 'c': -65.0, 'b': 0.2, 'd': 2.0, 'i_offset': 3.97,
         'u_init': -14.0, 'v_init': -70.0,
 
         'exc_a_tau':  0.5,  'exc_b_tau': 2.4, # Spike source
@@ -35,12 +38,27 @@ cell_params = { # L_V_Py
 
 pop_ex = p.Population(1, p.extra_models.IZK_curr_comb_exp_4E4I, cell_params,  label="test")
 
-# define projections
-exc_proj0 = p.Projection(pop_src2, pop_ex,
-        p.OneToOneConnector(weights=1, delays=100), target="inhibitory3")
 
+# AMPA
+k = 0.6
+# define projections
+exc_proj0 = p.Projection(pop_src3, pop_ex,
+        p.OneToOneConnector(weights=k*0.0132, delays=100), target="inhibitory4")
+
+# exc_proj1 = p.Projection(pop_src2, pop_ex,
+#         p.OneToOneConnector(weights=k*0.0275/2, delays=1), target="inhibitory3")
+#
 # exc_proj1 = p.Projection(pop_src1, pop_ex,
-#         p.OneToOneConnector(weights=200, delays=50), target="inhibitory4")
+#         p.OneToOneConnector(weights=k*0.33, delays=1), target="inhibitory2")
+#
+# exc_proj2 = p.Projection(pop_src1, pop_ex,
+#         p.OneToOneConnector(weights=k*0.33, delays=1), target="inhibitory2")
+#
+# exc_proj3 = p.Projection(pop_src1, pop_ex,
+#         p.OneToOneConnector(weights=k*0.33, delays=1), target="inhibitory2")
+
+# exc_proj3 = p.Projection(pop_src1, pop_ex,
+#         p.OneToOneConnector(weights=0.33, delays=1), target="inhibitory2")
 
 # # NMDA synapse
 # exc_proj2 = p.Projection(pop_src2, pop_ex,
@@ -55,15 +73,15 @@ t_total = 0
 p.run(t_before_pulses)
 t_total += t_before_pulses
 
-for i in range(num_pulses):
-    p.run(period/2)
-    pop_ex.set('i_offset', pop_ex.get('i_offset') + amplitude)
-    p.run(period/2)
-    pop_ex.set('i_offset', pop_ex.get('i_offset') - amplitude)
-    t_total += period
-
-p.run(t_after_pulses)
-t_total += t_after_pulses
+# for i in range(num_pulses):
+#     p.run(period/2)
+#     pop_ex.set('i_offset', pop_ex.get('i_offset') + amplitude)
+#     p.run(period/2)
+#     pop_ex.set('i_offset', pop_ex.get('i_offset') - amplitude)
+#     t_total += period
+#
+# p.run(t_after_pulses)
+# t_total += t_after_pulses
 # #######################
 
 v = pop_ex.get_v()
@@ -72,6 +90,5 @@ spikes = pop_ex.getSpikes()
 
 plot_utils.plotAll(v, spikes)
 plot_utils.plot_gsyn(curr)
-
-print "total time: {}".format(t_total)
 p.end()
+print "total time: {}".format(t_total)
