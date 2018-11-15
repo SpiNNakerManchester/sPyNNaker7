4246a7f364d8e30e069d55906ddf7f9401408a2b
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2018-11-15 10:52:33 +0000

    vrpss enabled

--- spynnaker7/pyNN/__init__.py
+++ spynnaker7/pyNN/__init__.py
@@ -39,8 +39,8 @@ from spynnaker.pyNN.models.spike_source.spike_source_from_file \
     import SpikeSourceFromFile
 from spynnaker.pyNN.models.spike_source.spike_source_poisson \
     import SpikeSourcePoisson
-# from spynnaker.pyNN.models.spike_source.spike_source_poisson_variable \
-#    import SpikeSourcePoissonVariable
+from spynnaker.pyNN.models.spike_source.spike_source_poisson_variable \
+   import SpikeSourcePoissonVariable
 from spynnaker.pyNN.models.utility_models.delay_extension_vertex \
     import DelayExtensionVertex
 from spynnaker.pyNN.utilities import utility_calls
